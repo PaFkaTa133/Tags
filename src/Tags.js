613f6b22713e9b323a4c689108bf24016473b9cc
PaFkaTa133 <119074272+PaFkaTa133@users.noreply.github.com>
2022-12-27 18:25:55 +0200

    did we?

---
+++ src/Tags.js
@@ -0,0 +1,17 @@
+function Tags(props) {
+
+    return (
+      <div className="tags">
+          {
+              props.tags.map((tag)=>{
+                  return <div className="tag">
+                      #{tag}
+                  </div>
+              })
+          }
+      </div>
+    );
+  }
+  
+  export default Tags;
+  
